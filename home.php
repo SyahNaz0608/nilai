992b3ca6442d6e457823f5f81583ec87022cb6cc
Muhamad Syah Gilbartar <muhamadsyahgilbartar@gmail.com>
2024-03-30 15:10:54 +0700

    tugas

---
+++ home.php
@@ -0,0 +1,10 @@
+<div class="card">
+    <div class="card-body">
+        <p>
+          <img src="./assets/code.jpg" alt="contoh gambar" class="rounded float-end">
+          <b>Web Programming</b> adalah istilah luas untuk pekerjaan yang terlibat dalam mengembangkan suatu situs web untuk internet (World Wide Web) atau intranet (jaringan pribadi)
+          <br>
+          <b>Student Day Web Programming</b> adalah salah satu student day yang ada di SMA Plus PGRI Cibinong yang mempersiapkan siswa/siswi mendapatkan keterampilan dalam bidang programming
+        </p>
+    </div>
+</div>
